dcce6bdd1f91a364976b5d3a2ca143dda26b9a35
Varunreddy <varunsannapureddy@gmail.com>
2025-07-15 03:56:34 +0530

    updated server.ts file

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,5 @@ const server = http.createServer(app);
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  console.log("Jai Shree Ram");
   logger.info(`Server is running on port ${PORT}`);
 });
