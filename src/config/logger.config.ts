a046a896f93674c036d5f69f08e71df8b20b001d
Varunreddy <varunsannapureddy@gmail.com>
2025-08-04 00:58:16 +0530

    enhance logger configuration to include error stack traces and separate error log file

--- src/config/logger.config.ts
+++ src/config/logger.config.ts
@@ -1,15 +1,23 @@
 import { createLogger, format, transports } from "winston";
 
-const { combine, timestamp, printf } = format;
+const { combine, timestamp, printf, errors } = format;
 
 const customFormat = printf(({ level, message, label, timestamp }) => {
   return `${timestamp}  ${level}: ${message}`;
 });
 
 export const logger = createLogger({
-  format: combine(timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), customFormat),
+  format: combine(
+    timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    errors({ stack: true }),
+    customFormat
+  ),
   transports: [
     new transports.Console(),
     new transports.File({ filename: "combines.log" }),
+    new transports.File({
+      filename: "error.log",
+      level: "error",
+    }),
   ],
 });
