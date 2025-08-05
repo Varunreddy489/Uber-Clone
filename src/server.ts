0cc94ada5bbcca7be4af4269e4613dc45f6a81ef
Varunreddy <varunsannapureddy@gmail.com>
2025-08-05 16:42:13 +0530

    add socket.io integration and initialize server with CORS settings

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
 import http from "http";
 import app from "./app";
 import { logger, PORT } from "./config";
+import { Server } from "socket.io";
+import { initSocket } from "./socket";
 
 const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CORS_ORIGIN,
+    methods: ["GET", "POST"],
+  },
+});
+
+initSocket(io);
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
