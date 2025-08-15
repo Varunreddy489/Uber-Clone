112825763d49557fef2509e3be568770cae24852
Varunreddy <varunsannapureddy@gmail.com>
2025-08-15 15:11:53 +0530

    refactor(auth): remove @clerk/express and associated middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import cors from "cors";
 import express from "express";
-import { clerkMiddleware } from "@clerk/express";
 
 import { limiter } from "./config";
 import { apiRoutes } from "./routes";
@@ -15,7 +14,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(limiter);
 app.use(express.json());
-app.use(clerkMiddleware());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
